b21a2b2fa1b6e7be148690d37c9bd0b7f08eda58
janiceh94 <haesung94@gmail.com>
2022-06-08 18:43:40 -0700

    installed react-router-dom and connected componenets to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <main>
-      <navbar/>
-      <skills/>
-      <projects/>
-      <about/>
-      <contact/>
+      <Navbar/>
+      <Skills/>
+      <Projects/>
+      <About/>
+      <Contact/>
     </main>
   );
 }
