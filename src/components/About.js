3259c48352c02c60fef3907029fa0a0365d1779b
janiceh94 <haesung94@gmail.com>
2022-06-08 18:47:33 -0700

    created navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
 export default function About(){
     return(
         <div className = 'about'>
-            <h1>
+            <h1>Janice</h1>
+            <h6>
                 I am a full stack software engineer who enjoys solving complex problems and writing code that it is easy to understand. With a background in Biology, I am a strategic thinker who is ready  to tackle complex problems by thoughtfully compiling necessary inputs while explaining difficult concepts to nontechnical audiences. I am passionate about creating applications that impact people in positive ways whether it be through healthcare or simply making an application that is more user friendly.  
-            </h1>
+            </h6>
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,10 @@
 export default function Navbar(){
-
-} 
+    return (
+        <div className='nav'>
+            <a href = '#about'>About</a>
+            <a href = '#skills'>Technical Skills</a>
+            <a href = '#projects'>Projects</a>
+            <a href = '#contact'>Contact</a>
+        </div>
+    )
+}
