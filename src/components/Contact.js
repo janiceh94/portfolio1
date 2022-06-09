fac69eb66ff53fc8d6d460e8a6f952d28386670f
janiceh94 <haesung94@gmail.com>
2022-06-08 18:59:22 -0700

    connected social media links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,8 @@ export default function Contact(){
                 <h2>Get in touch</h2>
                     <h6>Email: haesung94@gmail.com</h6>
                     <div className='contactLinks'>
-                        <a href = 'https://www.linkedin.com/in/janice-kwak/'><h6>LinkIn Profile</h6></a>
-                        <a href = 'https://github.com/janiceh94'><h6>GitHub Profile</h6></a>
+                        <a href = 'https://www.linkedin.com/in/janice-kwak/' target="_blank" rel="noopener noreferrer"><h6>LinkIn Profile</h6></a>
+                        <a href = 'https://github.com/janiceh94' target="_blank" rel="noopener noreferrer"><h6>GitHub Profile</h6></a>
                     </div>
             </div>
         </div>
