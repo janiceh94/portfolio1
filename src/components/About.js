fb08e2fc61c638a8d99abbd3097caefa89da0624
janiceh94 <haesung94@gmail.com>
2022-06-08 19:01:11 -0700

    added profile pic

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ export default function About(){
         <div id = 'about'>
             <h2>About Me</h2>
                 <div className = 'aboutMe'>
-                    <img alt = 'profile pic' src='#'></img>
+                    <img alt = 'profile pic' src='https://i.imgur.com/EvZjutx.jpg'>
+                    </img>
                     <h5>
                         I am a full stack software engineer who enjoys solving complex problems and writing code that it is easy to understand. With a background in Biology, I am a strategic thinker who is ready  to tackle complex problems by thoughtfully compiling necessary inputs while explaining difficult concepts to nontechnical audiences. I am passionate about creating applications that impact people in positive ways whether it be through healthcare or simply making an application that is more user friendly.  
                     </h5>
