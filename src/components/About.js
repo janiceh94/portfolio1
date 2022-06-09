49526e124b7e27be54c90e67f1a468882733c525
janiceh94 <haesung94@gmail.com>
2022-06-08 19:14:58 -0700

    about me section is responsive:

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,9 @@ export default function About(){
                 <div className = 'aboutMe'>
                     <img alt = 'profile pic' src='https://i.imgur.com/EvZjutx.jpg'>
                     </img>
-                    <h5>
+                    <h4>
                         I am a full stack software engineer who enjoys solving complex problems and writing code that it is easy to understand. With a background in Biology, I am a strategic thinker who is ready  to tackle complex problems by thoughtfully compiling necessary inputs while explaining difficult concepts to nontechnical audiences. I am passionate about creating applications that impact people in positive ways whether it be through healthcare or simply making an application that is more user friendly.  
-                    </h5>
+                    </h4>
                 </div>
         </div>
     )
