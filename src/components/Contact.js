add6a38ee7efeef5fcf023eef5e5366821df4311
janiceh94 <haesung94@gmail.com>
2022-06-08 19:08:39 -0700

    mobile responsive for project images

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,7 @@ export default function Contact(){
                     <div className='contactLinks'>
                         <a href = 'https://www.linkedin.com/in/janice-kwak/' target="_blank" rel="noopener noreferrer"><h6>LinkIn Profile</h6></a>
                         <a href = 'https://github.com/janiceh94' target="_blank" rel="noopener noreferrer"><h6>GitHub Profile</h6></a>
+                        <a href = 'https://docs.google.com/document/d/1meisDDtxLly5pyrftMdgUUkKjKyWCvfk11Elm1-5So0/edit?usp=sharing' target="_blank" rel="noopener noreferrer"><h6>Resume</h6></a>
                     </div>
             </div>
         </div>
