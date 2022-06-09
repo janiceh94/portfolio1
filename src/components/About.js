975cb629b807908a75dfc28517e2cd279a640e0a
janiceh94 <haesung94@gmail.com>
2022-06-08 18:58:45 -0700

    added social media links and hover effect

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
 export default function About(){
     return(
         <div id = 'about'>
-            <img alt = 'profile pic' src='#'></img>
-            <h6>
-                I am a full stack software engineer who enjoys solving complex problems and writing code that it is easy to understand. With a background in Biology, I am a strategic thinker who is ready  to tackle complex problems by thoughtfully compiling necessary inputs while explaining difficult concepts to nontechnical audiences. I am passionate about creating applications that impact people in positive ways whether it be through healthcare or simply making an application that is more user friendly.  
-            </h6>
+            <h2>About Me</h2>
+                <div className = 'aboutMe'>
+                    <img alt = 'profile pic' src='#'></img>
+                    <h5>
+                        I am a full stack software engineer who enjoys solving complex problems and writing code that it is easy to understand. With a background in Biology, I am a strategic thinker who is ready  to tackle complex problems by thoughtfully compiling necessary inputs while explaining difficult concepts to nontechnical audiences. I am passionate about creating applications that impact people in positive ways whether it be through healthcare or simply making an application that is more user friendly.  
+                    </h5>
+                </div>
         </div>
     )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,12 @@ export default function Contact(){
     return (
         <div id="contact">
             <div className='contactDetails'>
-                <h5>Get in touch</h5>
-                <h6>Email: haesung94@gmail.com</h6>
+                <h2>Get in touch</h2>
+                    <h6>Email: haesung94@gmail.com</h6>
+                    <div className='contactLinks'>
+                        <a href = 'https://www.linkedin.com/in/janice-kwak/'><h6>LinkIn Profile</h6></a>
+                        <a href = 'https://github.com/janiceh94'><h6>GitHub Profile</h6></a>
+                    </div>
             </div>
         </div>
     )
